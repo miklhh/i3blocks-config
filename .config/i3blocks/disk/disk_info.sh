01c663e8a59a9dbf352469ab95db80dfefe8fd53
miklhh <mike.zx@hotmail.com>
2019-01-13 23:43:29 +0100

    2019-01-13

--- .config/i3blocks/disk/disk_info.sh
+++ .config/i3blocks/disk/disk_info.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Disk info.
-DISK0="/dev/sda3"
-
 # Get the disk info.
-DISK_USAGE0=$(df -h | egrep $DISK0 | awk '{ printf("%4s/%s", $4, $2) }')
+DISK_USAGE0=$(df -h / | egrep "/" | awk '{ printf("%4s/%s", $4, $2) }')
 
 # Print the disk info.
 FLOOPY_EMOJI=$(echo -e "\U0001F4BE")
