451fd85b16a08bf789a521abe2500157a4344976
miklhh <mike.zx@hotmail.com>
2019-04-03 23:05:11 +0200

    Refactored weather blocklet 2019-04-03.

--- .config/i3blocks/weather/weather.py
+++ .config/i3blocks/weather/weather.py
@@ -2,8 +2,6 @@
 import requests
 import xml.etree.ElementTree as ET
 import datetime
-import sys
-import time
 import os
 
 # Forecast URL.
@@ -12,6 +10,7 @@ yr_url = "https://www.yr.no/place/Sverige/%C3%96sterg%C3%B6tland/Link%C3%B6ping/
 # Good to have data + funky emojicons.
 forecast_file = os.path.dirname(os.path.realpath(__file__)) + "/forecast.xml"
 date = datetime.datetime.now().strftime("%Y-%m-%d")
+
                                           # Day   #Night
 weather_types = { "Fair"                : ["☀️",   "🌙"],
                   "Partly cloudy"       : ["⛅",  "☁️"],
@@ -26,12 +25,13 @@ weather_types = { "Fair"                : ["☀️",   "🌙"],
                   "Foggy"               : ["🌫️",  "🌫️"],
                   "Light snow showers"  : ["🌨️",  "🌨️"]}
 
-# Get the data from YR helper function.
+# Get the data from YR and handle caching.
 def get_xml_root():
     yr_response = 0
     try:
         yr_response = requests.get(yr_url)
         if yr_response.status_code != 200:
+            # Bad response code.
             print("Error: YR status code " + str(yr_response.status_code))
             exit()
         else:
@@ -40,68 +40,69 @@ def get_xml_root():
                 f.write(yr_response.text)
             return ET.fromstring(yr_response.text)
     except requests.ConnectionError:
-        # Probably just no internet. Use latest forecast.
+        # Probably just no internet. Use cached forecast.
         if os.path.isfile(forecast_file):
             with open(forecast_file) as f:
                 yr_response = f.read()
-            # Print recycle emoji and return.
+            # Print recycle emoji and continue using cached forecast.
             print("(♻️)", end=" ")
             return ET.fromstring(yr_response)
         else:
+            # No internet and no cached forecast. This is a dead end.
             print("No forecast data.")
             exit()
-
-
-# Get forecast data from a forecast XML entry..
-def parse_forecast(forecast):
-    time_from =     forecast.attrib.get("from")
-    time_to =       forecast.attrib.get("to")
-    temp =          forecast.find("temperature").attrib.get("value")
-    weather =       forecast.find("symbol").attrib.get("name")
-    wind_dir =      forecast.find("windDirection").attrib.get("code")
-    wind_speed =    forecast.find("windSpeed").attrib.get("mps")
-    pressure =      forecast.find("pressure").attrib.get("value")
-    precipitaion =  forecast.find("precipitation").attrib.get("value")
-    return [time_from,  time_to,    temp,       weather, 
-            wind_dir,   wind_speed, pressure,   precipitaion]
-    
 
 # Get the XML root.
 xml_root = get_xml_root()
 
-# Parse the location.
-location = xml_root.find("location")
-location_name = location.find("name").text
-location_country = location.find("country").text
+# Parse the sun rise and set time. Appearntly, they are not always available and
+# so we need to make sure they exist in the recieved data.
+rise_fall_available = True
+sun_rise_time = sun_set_time = ""
+try:
+    sun_rise_time = xml_root.find("sun").attrib.get("rise");
+    sun_rise_time = sun_rise_time[ sun_rise_time.find('T') + 1 : len(sun_rise_time) - 3 ]
+    sun_set_time  = xml_root.find("sun").attrib.get("set");
+    sun_set_time  = sun_set_time[ sun_set_time.find('T') + 1 : len(sun_set_time) - 3 ]
+except:
+    rise_fall_available = False
 
-# Parse the sun rise and set time.
-sun_rise_time = xml_root.find("sun").attrib.get("rise");
-sun_rise_time = sun_rise_time[ sun_rise_time.find('T') + 1 : len(sun_rise_time) - 3 ]
-sun_set_time = xml_root.find("sun").attrib.get("set");
-sun_set_time = sun_set_time[ sun_set_time.find('T') + 1 : len(sun_set_time) - 3 ]
-
-# One forecast. The user wants it on short form.
-forecast_now = xml_root.find("forecast").find("tabular").find("time")
-[time_from, time_to,    
- temp,      weather, 
- wind_dir,  wind_speed, 
- pressure,  precipitation] = parse_forecast(forecast_now);
+# Get the current weather information.
+forecast = xml_root.find("forecast").find("tabular").find("time")
+weather = forecast.find("symbol").attrib.get("name")
+temperature = forecast.find("temperature").attrib.get("value")
+wind_direction = forecast.find("windDirection").attrib.get("code")
+wind_speed = forecast.find("windSpeed").attrib.get("mps")
+precipitation = forecast.find("precipitation").attrib.get("value")
 
 # Night time?
-sun_rise = datetime.datetime.strptime(sun_rise_time, "%H:%M")
-sun_set = datetime.datetime.strptime(sun_set_time, "%H:%M")
+night = 0
 now = datetime.datetime.now()
-night = 1 if now.time() < sun_rise.time() or sun_set.time() < now.time() else 0
+if rise_fall_available:
+    # Use sun rise and fall time to determine.
+    sun_rise = datetime.datetime.strptime(sun_rise_time, "%H:%M")
+    sun_set = datetime.datetime.strptime(sun_set_time, "%H:%M")
+    night = 1 if now.time() < sun_rise.time() or sun_set.time() < now.time() else 0
+else:
+    # No rise/fall time available. Approximate daytime as [07:00 - 21:00]
+    sun_rise = datetime.datetime.strptime("07:00", "%H:%M")
+    sun_set = datetime.datetime.strptime("21:00", "%H:%M")
+    night = 1 if now.time() < sun_rise.time() or sun_set.time() < now.time() else 0
 
 # Print the weather.
 if weather in weather_types:
+    # Emoji avaiable for usage.
     print(weather + ": " + weather_types.get(weather)[night] + " ", end="")
 else:
+    # No emoji available, use regular text.
     print(weather + " ", end="")
 
 # Print the temperature and sun times.
-print(temp + "\u00B0C", end=" ")
-print ("[" + sun_rise_time + " " + "🌅" + " " + sun_set_time + "]", end=" ")
+print(temperature, end="°C ")
+
+# Print the sun rise and set time.
+if rise_fall_available:
+    print ("[" + sun_rise_time + " 🌅 " + sun_set_time + "]", end=" ")
 
 # Print the precipitation (if there is any).
 if precipitation != "0":
@@ -109,4 +110,4 @@ if precipitation != "0":
     print("| ☔ " + precipitation + "mm", end=" ")
 
 # Print wind data.
-print("| 🍃 " + wind_speed + "m/s " + "(" + wind_dir + ")");
+print("| 🍃 " + wind_speed + "m/s " + "(" + wind_direction + ")");
