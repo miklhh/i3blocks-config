4109dd278746be5746993b0246c30b253bb2229b
miklhh <mike.zx@hotmail.com>
2019-05-23 21:01:20 +0200

    Removed CPU-STAT dependencie.

--- .config/i3blocks/cpu/cpu_info.sh
+++ .config/i3blocks/cpu/cpu_info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 TEMP=$(sensors | awk '/Package id 0:/{ printf("%s", $4) }')
-USAGE=$(~/.config/i3blocks/cpu/cpu-stat)
-echo "$USAGE" "$TEMP" | awk '{ printf(" CPU:%6s% @ %s "), $1, $2 }'
+USAGE=$(mpstat 1 1 | awk '/:[0-9]{2}.*all/ {printf("%s\n", $4)}')
+echo "$USAGE $TEMP" | awk '{ printf(" CPU:%6s% @ %s "), $1, $2 }'
