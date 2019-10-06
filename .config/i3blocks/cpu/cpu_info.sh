a30fde04539fefbfe6fee0e6cfbedefed9fdcd0c
miklhh <mike.zx@hotmail.com>
2019-10-06 18:32:20 +0200

    Added more robust CPU usage acquiring technique.

--- .config/i3blocks/cpu/cpu_info.sh
+++ .config/i3blocks/cpu/cpu_info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 TEMP=$(sensors | awk '/Package id 0:/{ printf("%s\n", $4) }')
-USAGE=$(mpstat 1 1 | awk '/:[0-9]{2}.*all/ {printf("%s\n", $4)}')
+USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
 echo "$USAGE $TEMP" | awk '{ printf(" CPU:%6s% @ %s \n"), $1, $2 }'
